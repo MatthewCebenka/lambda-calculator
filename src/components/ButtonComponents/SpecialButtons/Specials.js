dc481a0cb2da34188473f64a85a6b293d3d028e1
Matthew Cebenka <matthewt.cebenka@gmail.com>
2019-11-08 09:13:53 -0500

    Almost MVP

--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,19 +1,25 @@
-import React from "react";
-import { Specials } from '../../../data';
-import { from } from "rxjs";
+import React, {useState} from "react";
+import { specials } from '../../../data';
+import  SpecialButton  from './SpecialButton'
+// import { from } from "rxjs";
 //import any components needed
 
 //Import your array data to from the provided data file
 
 const Specials = () => {
   // STEP 2 - add the imported data to state
-  const [specialsState, setSpecialsState] = useState(Specials);
+  const [specialsState, setSpecialsState] = useState(specials);
 
   return (
     <div>
+      {specialsState.map((special, i) => (
+        <SpecialButton special={special}/>
+      ))}
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
     </div>
   );
 };
+
+export default Specials;
